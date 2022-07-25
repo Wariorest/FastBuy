be2405fdfbfc948fa231a98ade9070877a91c1a7
Vladislav Martynov <Wariorest@gmail.com>
2022-07-25 19:02:48 +0300

    added db parameters to env added sequelize db ovj added db connetion in index.js

--- server/db.js
+++ server/db.js
@@ -1,8 +1,15 @@
 import {Sequelize} from "sequelize";
+import dotenv from "dotenv";
+dotenv.config()
 
-
-
-
-
-
-export default new Sequelize();
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    }
+);
+export default sequelize;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import sequelize from "./db.js"
+
 
 dotenv.config()
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-async function startApp(){
+
+
+
+
+const startApp = async ()=>{
     try{
+        await sequelize.authenticate();
+        await sequelize.sync();
+        
         app.listen(PORT, () => {
             console.log('server started on port ' + PORT);
         })
