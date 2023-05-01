a74511cc0f301ccc92d097776b0db9c6d7e48a0c
Vladyslav Martynov <wariorest@gmail.com>
2023-05-02 01:34:48 +0200

    finished NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,26 +1,30 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
-import {Button, Container, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+import {SHOP_ROUTE} from "../utils/consts";
+import {observer} from "mobx-react-lite";
 
-const NavBar = () => {
+const NavBar = observer(() => {
     const {user} = useContext(Context);
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                    <Button variant="outline-light">Search</Button>
-                </Form>
+                <NavLink style={{color: "white"}} to={SHOP_ROUTE}>FastBuy</NavLink>
+                {user.isAuth ?
+                    <Nav className="ms-auto" style={{color: "white"}}>
+                        <Button variant={"outline-light"}>Admin panel</Button>
+                        <Button variant={"outline-light"} className={"ms-2"}>Sign out</Button>
+                    </Nav>
+                    :
+                    <Nav className="ms-auto" style={{color: "white"}}>
+                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Authorization</Button>
+                    </Nav>
+                }
             </Container>
         </Navbar>
 
     );
-};
+});
 
 export default NavBar;
