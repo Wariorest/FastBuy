e60fcc4c23b0a145a42c07451b6c75d1d37cc20c
Vladyslav Martynov <wariorest@gmail.com>
2023-05-03 01:08:58 +0200

    Added BrandBar.js

---
+++ client/src/components/BrandBar.js
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react';
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {Card, Container, Row} from "react-bootstrap";
+
+const BrandBar = observer (() => {
+    const {device} = useContext(Context);
+    return (
+        <Container className="d-flex flex-wrap">
+            {device.brands.map((brand)=>
+                <Card
+                    key={brand.id}
+                    className="p-3"
+                    onClick={()=> device.setSeectedBrand(brand)}
+                    border={ brand.id === device.selectedBrand.id ? "dark" : "light"}
+                    style={{cursor:"pointer"}}
+                >
+                    {brand.name}
+                </Card>
+            )}
+        </Container>
+    );
+});
+
+export default BrandBar;
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import TypeBar from "../components/TypeBar";
+import BrandBar from "../components/BrandBar";
 
 const Shop = () => {
     return (
         <Container>
-          <Row className="mt-2">
-              <Col md={3}>
-                <TypeBar/>
-              </Col>
-              <Col md={9}>
-
-              </Col>
-          </Row>
+            <Row className="mt-2">
+                <Col md={3}>
+                    <TypeBar/>
+                </Col>
+                <Col md={9}>
+                    <BrandBar/>
+                </Col>
+            </Row>
         </Container>
     );
 };
