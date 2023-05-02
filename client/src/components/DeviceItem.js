a8bd1f0e9013e66a52f8c47583e88f5dc4378146
Vladyslav Martynov <wariorest@gmail.com>
2023-05-03 01:56:40 +0200

    Added DeviceList.js Added DeviceItem.js

---
+++ client/src/components/DeviceItem.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Card, Col, Image} from "react-bootstrap";
+import star from '../assets/raiting_star.png';
+import {useNavigate} from "react-router-dom";
+import {DEVICE_ROUTE} from "../utils/consts";
+const DeviceItem = ({device}) => {
+    const navigate = useNavigate();
+    console.log(navigate)
+    return (
+        <Col md={3} className={"mt-3"} onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)} >
+            <Card
+                style={{width:150, cursor:'pointer'}}
+                border={"light"}
+            >
+                <Image
+                    src={device.img}
+                    width={150}
+                    height={150}
+                />
+                <div className={"text-black-50 d-flex justify-content-between align-items-center mt-2"}>
+                    <div>
+                        Test item for selling...
+                    </div>
+                    <div className={"d-flex align-items-center"}>
+                        <div>{device.rating}</div>
+                        <Image
+                            src={star}
+                            width={14}
+                            height={14}
+                        />
+                    </div>
+                </div>
+                <div>{device.name}</div>
+            </Card>
+        </Col>
+    );
+};
+
+export default DeviceItem;
