0a194b528042abb3ebbd7bd57831777e3f0f39a6
Vladyslav Martynov <wariorest@gmail.com>
2023-05-02 02:38:58 +0200

    added component Auth.js

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -6,7 +6,7 @@ import {Context} from "../index";
 
 const AppRouter = () => {
     const {user} = useContext(Context);
-    console.log(user);
+    //console.log(user);
     return (
         <Switch>
             { user.isAuth === true && authRoutes.map(({path, Component}) =>
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import {Button, Card, Container, Form} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE
+    console.log(location)
     return (
-        <div>
-            Auth
-        </div>
+        <Container
+            className={"d-flex justify-content-center align-items-center"}
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? "Authorization" : "Sign up"}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Email"
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Password"
+                    />
+                    <Button
+                        className="mt-3"
+                        variant="outline-dark"
+                    >
+                        {isLogin? "Sign in": "Sign up"}
+
+                    </Button>
+                    {isLogin ?
+                        <div className="mt-3">
+
+                            Don't have account? <NavLink to={REGISTRATION_ROUTE}>
+                            Sign up! </NavLink>
+                        </div>
+                        :
+                        <div className="mt-3">
+
+                            Have account? <NavLink to={LOGIN_ROUTE}>
+                            Sign in! </NavLink>
+                        </div>
+                    }
+                </Form>
+            </Card>
+
+        </Container>
     );
 };
 
