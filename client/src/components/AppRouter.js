2058b90eff518edaa99da2f40e9d95daa0129f3e
Vladyslav Martynov <wariorest@gmail.com>
2023-05-02 06:52:53 +0200

    updated npm fixed AppRouter.js fixed Auth.js

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,24 +1,31 @@
 import React, {useContext} from 'react';
-import {Switch, Route, Redirect} from "react-router-dom";
-import {authRoutes, publicRoutes} from "../routes";
-import {SHOP_ROUTE} from "../utils/consts";
+
+import {
+    Routes,
+    Route
+} from "react-router-dom";
+import {authRoutes,publicRoutes} from "../routes";
 import {Context} from "../index";
+import {observer} from "mobx-react-lite";
+import Shop from "../pages/Shop";
 
-const AppRouter = () => {
-    const {user} = useContext(Context);
-    //console.log(user);
-    return (
-        <Switch>
-            { user.isAuth === true && authRoutes.map(({path, Component}) =>
-                <Route key = {path} path = {path} component = {Component} exact/>
-            )}
 
-            { publicRoutes.map(({path, Component}) =>
-                <Route key = {path} path = {path} component = {Component} exact/>
+const AppRouter = observer(() =>  {
+    const {device, user} = useContext(Context)
+    //console.log(user)
+   // console.log(device)
+    return(
+        <Routes>
+            {user._isAuth && authRoutes.map(({path, Component}) =>
+                <Route key = {path}  path={path} element={<Component/>} exact/>
             )}
-            <Redirect to = {SHOP_ROUTE} />
-        </Switch>
+            {publicRoutes.map(({path , Component}) =>
+                <Route key = {path} path = {path} element={<Component/>} exact/>
+            )}
+            <Route path = "*" element={<Shop/>}/>
+        </Routes>
+
     );
-};
+});
 
 export default AppRouter;
