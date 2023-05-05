aaf1272145dab6d66ae465798270c9fa970e6abf
Vladyslav Martynov <wariorest@gmail.com>
2023-05-05 23:02:29 +0200

    Added http folder, .env, index.js with base url, $host, $authHost and interceptors

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import DeviceStore from "./store/DeviceStore";
 export const Context = createContext(null);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
     <React.StrictMode>
         <Context.Provider value={{
