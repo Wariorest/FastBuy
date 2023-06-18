86570488311b14bbfc899d23c05ae2430abf6243
Vladyslav Martynov <wariorest@gmail.com>
2023-06-19 00:16:14 +0300

    Orders list part

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -3,11 +3,13 @@ import {Button, Container} from "react-bootstrap";
 import CreateType from "../components/modal/CreateType";
 import CreateBrand from "../components/modal/CreateBrand";
 import CreateDevice from "../components/modal/CreateDevice";
+import ShowOrders from "../components/modal/ShowOrders";
 
 const Admin = () => {
     const [typeVisible, setTypeVisible] = useState(false);
     const [brandVisible, setBrandVisible] = useState(false);
     const [deviceVisible, setDeviceVisible] = useState(false);
+    const [orderListVisible, setOrderListVisible] = useState(false);
     return (
         <Container className={"d-flex flex-column"}>
            <Button
@@ -31,9 +33,17 @@ const Admin = () => {
            >
                Add device
            </Button>
+            <Button
+               variant={"outline-dark"}
+               className={"mt-4 p-2"}
+               onClick={()=> setOrderListVisible(true)}
+           >
+               Show Orders
+           </Button>
             <CreateType show={typeVisible} onHide={()=> setTypeVisible(false)}/>
             <CreateBrand show={brandVisible} onHide={()=> setBrandVisible(false)}/>
             <CreateDevice show={deviceVisible} onHide={()=> setDeviceVisible(false)}/>
+            <ShowOrders show={orderListVisible} onHide={()=> setOrderListVisible(false)}/>
         </Container>
     );
 };
